1f457dcfe87483e261af3d5d841c7d5a3d4df4a5
Ondřej Hruška <ondra@ondrovo.com>
2015-06-16 22:02:55 +0200

    added ciba43 to "waiting list" for stacking

--- js/people.js
+++ js/people.js
@@ -2,10 +2,12 @@ var people = [
 	{name: '@MightyPork', tz: 'Europe/Prague', color: '#EAF425'},
 	{name: '@lastofavari', tz: 'Europe/Minsk', color: '#4ECEFF'},
 
+	// GMT+2 :(
 	//{name: '@Locercus', tz: 'Europe/Copenhagen', color: '#EAF425'},
 	//{name: '@Creeper32605', tz: 'Europe/Copenhagen', color: '#EAF425'}, // (city?)
 	//{name: '@JesperJacoben', tz: 'Europe/Copenhagen', color: '#EAF425'},
 	//{name: '@Jaredlll08', tz: 'Africa/Johannesburg', color: '#EAF425'},
+	//{name: '@ciba43', tz: 'Europe/Riga', color: '#EAF425'},
 
 	{name: '@TheBadFame', tz: 'America/Mexico_City', color: '#329AFF'},
 	{name: '@ImReble548', tz: 'America/Denver', color: '#69E79E'},
