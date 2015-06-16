46b29fa47e410d1b0d7d52b7e0629780dc2262bd
Ondřej Hruška <ondra@ondrovo.com>
2015-06-16 21:28:19 +0200

    moved people to separate file, renamed css and js files

---
+++ js/people.js
@@ -0,0 +1,16 @@
+var people = [
+	{name: '@MightyPork', tz: 'Europe/Prague', color: '#EAF425'},
+	{name: '@lastofavari', tz: 'Europe/Minsk', color: '#4ECEFF'},
+
+	//{name: '@Locercus', tz: 'Europe/Copenhagen', color: '#EAF425'},
+	//{name: '@Creeper32605', tz: 'Europe/Copenhagen', color: '#EAF425'}, // (city?)
+	//{name: '@JesperJacoben', tz: 'Europe/Copenhagen', color: '#EAF425'},
+	//{name: '@Jaredlll08', tz: 'Africa/Johannesburg', color: '#EAF425'},
+
+	{name: '@TheBadFame', tz: 'America/Mexico_City', color: '#329AFF'},
+	{name: '@ImReble548', tz: 'America/Denver', color: '#69E79E'},
+	{name: '@Deli731234', tz: 'America/Tijuana', color: '#F57126'},
+
+	// just to show an Australian account
+	{name: '@eevblog', tz: 'Australia/Sydney', color: '#E63E3F'}
+];
